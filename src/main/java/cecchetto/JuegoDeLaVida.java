7ce7a035024ed7b00e66f9faa65dfccc0cecd702
RamiroCecchetto <ramirocecchetto123gmail.com>
2024-05-17 09:11:24 -0300

    Panel ahora es una clase anónima

--- src/main/java/cecchetto/JuegoDeLaVida.java
+++ src/main/java/cecchetto/JuegoDeLaVida.java
@@ -2,11 +2,12 @@ package cecchetto;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class JuegoDeLaVida extends JFrame {
 
     private PanelControl panelControl;
-    Panel panel;
     Celula[][] celulas;
     private int tamanio = 12;
     static int size = 600;
@@ -14,52 +15,40 @@ public class JuegoDeLaVida extends JFrame {
     public JuegoDeLaVida() {
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setResizable(false);
-        this.setVisible(true);
         this.setBackground(Color.DARK_GRAY);
         this.setSize(new Dimension(size, size));
-        this.setLocationRelativeTo(null);
-        panel = new Panel(this);
-        this.add(panel);
-        celulas = new Celula[tamanio][tamanio];
+        this.setLocationRelativeTo(null);       //cosas para que el Frame sea usable
+        panelControl = new PanelControl(this);
 
+        celulas = new Celula[tamanio][tamanio];
         for (int i = 0; i < tamanio; i++) {
             for (int j = 0; j < tamanio; j++) {
                 celulas[i][j] = new Celula(new Point(i, j), tamanio);
             }
         }
 
-        panelControl = new PanelControl(this);
+        this.add(new JPanel() {         //clase anónima para detectar el mouse y dibujar la escena y detectar el mouse
+            @Override
+            public void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                Graphics2D g2 = (Graphics2D) g;
+                dibujar(g2);
+                g2.dispose();
+            }
+        });
+        this.setVisible(true);  //Si no lo pongo acá no funcinoa
+
     }
 
-    public void dibujar(Graphics2D g) {
+    private void dibujar(Graphics2D g) {     //dibuja la escena
         for (Celula[] celula1 : celulas) {
             for (Celula celula : celula1) {
-                g.setColor(celula.getEstado().equals(Estado.viva) ? Color.WHITE : Color.BLACK);
-                g.fill(celula.getGrafico());
+                g.setColor(celula.getEstado().equals(Estado.viva) ? Color.WHITE : Color.BLACK); //pinta las celulas
+                g.fill(celula.getGrafico());                                                     // depentiendo de su estado
             }
         }
-
-
-
     }
 
-
-    private static class Panel extends JPanel {
-        private JuegoDeLaVida juegoDeLaVida;
-        public Panel(JuegoDeLaVida juegoDeLaVida) {
-            this.juegoDeLaVida = juegoDeLaVida;
-        }
-
-        @Override
-        protected void paintComponent(Graphics g) {
-            super.paintComponent(g);
-            Graphics2D g2 = (Graphics2D) g;
-
-            juegoDeLaVida.dibujar(g2);
-
-            g2.dispose();
-        }
-    }
 
 }
 
