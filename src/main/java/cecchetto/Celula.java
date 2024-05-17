0a11a1c72ba57e20f0290b34f78ea2d721fa9267
RamiroCecchetto <ramirocecchetto123gmail.com>
2024-05-17 08:37:27 -0300

    Las celdas se muestran en la ventana

--- src/main/java/cecchetto/Celula.java
+++ src/main/java/cecchetto/Celula.java
@@ -1,7 +1,36 @@
 package cecchetto;
 
+import java.awt.*;
+import java.awt.geom.Rectangle2D;
+
 public class Celula {
 
+    private Estado estado = Estado.muerta;
+    private Point posicion;
+    private int tamanio;
+    private int WHIDTH = 50;
+    private int HEIGTH = 50;
 
+    public Celula(Point posicion, int tamanio) {
+        this.posicion = posicion;
+        this.tamanio = tamanio;
+    }
 
+    public Rectangle2D getGrafico() {
+        int x = posicion.x * WHIDTH;
+        int y = posicion.y * WHIDTH;
+        return new Rectangle2D.Double(x ,y, WHIDTH, HEIGTH);
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+
+    public Point getPosicion() {
+        return posicion;
+    }
 }
--- src/main/java/cecchetto/JuegoDeLaVida.java
+++ src/main/java/cecchetto/JuegoDeLaVida.java
@@ -6,17 +6,76 @@ import java.awt.*;
 public class JuegoDeLaVida extends JFrame {
 
     private PanelControl panelControl;
+    Panel panel;
+    Celula[][] celulas;
+    private int tamanio = 12;
+    static int size = 600;
 
     public JuegoDeLaVida() {
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setResizable(false);
         this.setVisible(true);
         this.setBackground(Color.DARK_GRAY);
-
-        this.setSize(new Dimension(600, 600));
+        this.setSize(new Dimension(size, size));
         this.setLocationRelativeTo(null);
+        panel = new Panel(this);
+        this.add(panel);
+        celulas = new Celula[tamanio][tamanio];
+
+        for (int i = 0; i < tamanio; i++) {
+            for (int j = 0; j < tamanio; j++) {
+                celulas[i][j] = new Celula(new Point(i, j), tamanio);
+            }
+        }
 
         panelControl = new PanelControl(this);
     }
 
+    public void dibujar(Graphics2D g) {
+        for (Celula[] celula1 : celulas) {
+            for (Celula celula : celula1) {
+                g.setColor(celula.getEstado().equals(Estado.viva) ? Color.WHITE : Color.BLACK);
+                g.fill(celula.getGrafico());
+            }
+        }
+
+
+
+    }
+
+
+    private static class Panel extends JPanel {
+        private JuegoDeLaVida juegoDeLaVida;
+        public Panel(JuegoDeLaVida juegoDeLaVida) {
+            this.juegoDeLaVida = juegoDeLaVida;
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            Graphics2D g2 = (Graphics2D) g;
+
+            juegoDeLaVida.dibujar(g2);
+
+            g2.dispose();
+        }
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
